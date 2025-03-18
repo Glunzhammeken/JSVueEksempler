70d7e5ce369ace7947549fba37d835d64e0ed7d5
Glunzhammeken <160604689+Glunzhammeken@users.noreply.github.com>
2025-03-18 09:43:54 +0100

    lidt ekstra js om events på skærmen. Bools som viser og ikke cviser.
    v-mode.lazy

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ Vue.createApp({
                 { title: "The Lord of the Rings", price: 19.99 },
                 { title: "Harry Potter", price: 9.99 },
                 { title: "The Hobbit", price: 14.99 }
-            ]
+            
+            ],
+            counter: 0
 
             
         }
@@ -25,6 +27,14 @@ Vue.createApp({
     },
     
     methods: {
+        clickCounter()
+        {
+            this.counter++
+        },
+        clickCounter2(c)
+        {
+            return c+1
+        }
 
     }
 
