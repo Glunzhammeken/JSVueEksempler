27f13c1812ade29179b1338595b47682760ecc41
Glunzhammeken <160604689+Glunzhammeken@users.noreply.github.com>
2025-03-18 11:32:56 +0100

    penis clicker

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ Vue.createApp({
             name: "André",
             age: 25,
             atSchool: true,
-            book:{
+            book: {
                 title: "The Lord of the Rings",
                 price: 19.99
             },
@@ -17,24 +17,97 @@ Vue.createApp({
                 { title: "The Lord of the Rings", price: 19.99 },
                 { title: "Harry Potter", price: 9.99 },
                 { title: "The Hobbit", price: 14.99 }
-            
-            ],
-            counter: 0
 
-            
+            ],
+            counter: 0,
+            date: new Date(),
+            SU: 0,
+            Movies: [
+
+            ],
+
+            title: "",
+            price: 0,
+            rating: 0,
+            email: "",
+            password: "",
+
+            User: {
+                email: "",
+                password: "",
+            },
+            Users: [
+            ],
+
+            messagecockring: "",
+            interval: null,
+            amountNeededforring: 5,
+            amountNeededfordildo: 100,
+            pace: 1000,
+            cockringcounter: 0,
         }
+
 
     },
-    
     methods: {
-        clickCounter()
-        {
+        clickCounter() {
             this.counter++
         },
-        clickCounter2(c)
-        {
-            return c+1
-        }
+        counterreset() {
+            this.counter = 0
+        },
+        clickCounter2(c) {
+            return c + 1
+        },
+        AddMovie() {
+            this.Movies.push({ title: this.title, price: this.price, rating: this.rating })
+        },
+        AddUser() {
+            this.Users.push({ email: this.email, password: this.password })
+            this.email = ""
+            this.password = ""
+            this.message = "User Added"
+        },
+        mounted() {
+            if (this.amountNeededforring <= this.counter) {
+                this.counter = this.counter - this.amountNeededforring
+                this.amountNeededforring = this.amountNeededforring * 2
+                this.cockringcounter++
+                this.interval = setInterval(() => {
+                    this.counter++;
+                }, this.pace);
+
+            }
+            else {
+                this.messagecockring = "Cant afford"
+            }
+        },
+            beforeUnmount() {
+                
+                clearInterval(this.interval);
+                this.amountNeededforring = 5;
+                
+              },
+              mounteddildo() {
+                if (this.amountNeededfordildo <= this.counter) {
+                    this.counter = this.counter - this.amountNeededfordildo
+                    this.amountNeededfordildo = this.amountNeededfordildo * 2
+                    this.cockringcounter++
+                    this.interval = setInterval(() => {
+                        this.counter++;
+                    }, this.pace);
+    
+                }
+                else {
+                    this.messagecockring = "Cant afford"
+                }
+            },
+
+
+        
+    
+
+
 
     }
 
